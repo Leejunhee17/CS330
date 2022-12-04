c5997b64b9c01891d336041cdefb2dd4e177ea3c
leejunhee17 <jun17@kaist.ac.kr>
2022-12-04 06:49:23 +0000

    dir_vine 컷~!

--- filesys/filesys.c
+++ filesys/filesys.c
@@ -86,7 +86,7 @@ filesys_create (const char *name, off_t initial_size, bool is_dir) {
 		dir_close (new_dir);
 	}
 	
-	if (!success && inode_sector != 0)
+	if (!success && inode_clst != 0)
 		fat_remove_chain (inode_clst, 0); 
 #else
 	struct dir *dir = dir_open_root ();
